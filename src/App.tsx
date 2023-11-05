0b22fb49df973637fefe70bf4d430dd7531308bd
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2023-11-05 10:50:52 -0300

    update time with date-fns

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,44 @@ import styles from './App.module.css';
 
 import './global.css';
 
+// author: { avatar_url: "", name:"", role:"" }
+// publishedAt: Date
+// content: string
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/jieff.png',
+      name: 'Jieff Cavalcanti',
+      role: 'Developer @ksys and TnpBank'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+      //{type: 'link', content: '#novoprojeto'},
+      //{type: 'link', content: '#nlw '},
+      //{type: 'link', content: '#rocketseat'}
+    ],
+    publishedAt: new Date('2023-11-04 10:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/bezalelmiranda.png',
+      name: 'Bezalel Miranda',
+      role: 'FullStack Developer @ksys'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-11-05 10:00:00')
+  }
+];
 
 function App() {
 
@@ -16,8 +54,14 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+         {posts.map(post => {
+          return (
+            <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}              
+            />)
+         })}
         </main>
       </div>
 
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,31 +1,41 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css'
 
-export function Post(){
+export function Post({ author, publishedAt }){
+
+    const publisedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/jieff.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Jieff Cavalcanti</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="4 de novembro de 2023" dateTime="2023-11-04 23:54:30">Publicado há 1h</time>
+                <time 
+                    title={publisedDateFormatted}
+                    dateTime={publishedAt.toISOString()}>
+                        {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p> 
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '} <a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw </a>{' '}
-                    <a href="">#rocketseat</a>
-                </p>
+            
             </div>
 
 
