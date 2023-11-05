3153dbc56245ba764057c627f7b7305ab5b64dfc
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2023-11-05 00:25:50 -0300

    update post

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import igniteLogo from '../assets/ignite-logo.svg';
 export function Header(){
     return (
         <header className={styles.header}>
-            <img src={igniteLogo} alt="Logotipo do ignite" />
+            <img src={igniteLogo} alt="Logotipo" />
         </header>
         
     );
